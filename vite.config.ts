bad898d7b4101779e2347a511f95154070ebd352
Kishan Singh <kishaniam401@gmail.com>
2026-08-23 21:42:53 +0530

    fix vite deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(process.cwd(), "src"),
     },
   },
+
   build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "EaseUI",
-      fileName: (format) => `easeui.${format}.js`,
-    },
     cssCodeSplit: true,
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
   },
 });
